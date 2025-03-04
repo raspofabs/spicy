24bbb5bb76fd660b5531663fb472ec1af659c7ad
Richard Fabian <raspo1@gmail.com>
2025-03-04 21:33:18 +0000

    spicy start spec rendering

--- src/spicy/__init__.py
+++ src/spicy/__init__.py
@@ -1,6 +1,59 @@
 """Spicy is like needs, but for mdbook."""
 
+import sys
+from pathlib import Path
+from typing import Callable, List, Optional
 
-def main() -> None:
-    """Exclaim."""
-    print("Hello from spicy!")
+import click
+
+from .spec import SpecElement, gather_spec_elements
+
+
+def get_spec_files(root_path: Optional[Path] = None) -> List[Path]:
+    """Fetch a list of all the use-case files under a root path."""
+    glob_root = root_path or Path("src/02_spec")
+    if glob_root.is_file():
+        return [glob_root]
+    return sorted(glob_root.glob("*.md"))
+
+
+def get_specs_from_files(file_paths: List[Path]) -> List[SpecElement]:
+    """Return the combined use cases from all the md files."""
+    specs: List[SpecElement] = []
+
+    for filename in file_paths:
+        specs.extend(gather_spec_elements("CDU", filename))
+    return specs
+
+
+def render_issues(specs: List[SpecElement], render_function: Optional[Callable] = None):
+    """Render unresolved issues for each use-case."""
+    render_function = render_function or print
+    any_errors = False
+    for spec in specs:
+        if spec.render_issues():
+            any_errors = True
+    if not any_errors:
+        render_function("No issues found.")
+    return any_errors
+
+
+@click.command()
+@click.argument("path-override", required=False, default=None, type=Path)
+def run(
+    path_override: Optional[Path],
+):
+    """Find paths to read, then print out the TCLs of all the use-cases."""
+    if path_override is not None:
+        filenames = get_spec_files(path_override)
+    else:
+        filenames = get_spec_files()
+
+    print(f"Have {len(filenames)} files to read.")
+    specs = get_specs_from_files(filenames)
+    if render_issues(specs):
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    run()
