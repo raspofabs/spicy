8a472d82e128df1693114e655a09350b25d3c854
Richard Fabian <raspo1@gmail.com>
2025-03-05 19:46:46 +0000

    add spec check

--- run_spec_check.sh
+++ run_spec_check.sh
@@ -2,9 +2,9 @@
 
 while true; do
     # rerun script whenever anything changes in the spicy or tests folders
-    find src/spicy tests | sed '/__pycache__/d' > .test_files
-    cat .test_files | entr -d uv run tests/run.sh ;
-    rm .test_files
+    find src/spicy | sed '/__pycache__/d' > .spec_check_files
+    cat .spec_check_files | entr -d uv run spicy ../../src
+    rm .spec_check_files
     # sleep for four seconds so we have a chance to quit
     sleep 4
 done
