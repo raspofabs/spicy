47da899859a72565d4ad305c4dc2753cd91c7bae
Richard Fabian <raspo1@gmail.com>
2025-04-05 18:08:17 +0100

    adding initial stakeholder needs for SPICY

---
+++ run_self_check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do
+    # rerun script whenever anything changes in the spicy or tests folders
+    find src/spicy docs | sed '/__pycache__/d' > .spec_check_files
+    cat .spec_check_files | entr -d bash -c "clear && uv run spicy docs"
+    rm .spec_check_files
+    # sleep for four seconds so we have a chance to quit
+    sleep 4
+done
