a48e0ecf1c20b9e06171dfc3048b9bd549c065dd
Richard Fabian <raspo1@gmail.com>
2025-04-06 14:26:00 +0100

    adding mypy runner

---
+++ run_mypy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do
+    # rerun script whenever anything changes in the spicy or tests folders
+    find src/spicy tests | sed '/__pycache__/d' > .ruff_files
+    cat .ruff_files | entr -d bash -c "clear && uvx mypy src/spicy tests"
+    rm .ruff_files
+    # sleep for four seconds so we have a chance to quit
+    sleep 4
+done
