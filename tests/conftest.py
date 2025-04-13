d07e3367c5f85a93ba6a96fa364730cab401144f
Richard Fabian <raspo1@gmail.com>
2025-04-13 15:44:07 +0100

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,12 +12,12 @@ def test_data_path() -> Path:
 
 
 @pytest.fixture
-def positive_test_data_path() -> Path:
+def positive_test_data_path(test_data_path) -> Path:
     """Return the path to the test data directory."""
-    return Path(__file__).parent / "simple_test_spec"
+    return test_data_path / "simple_test_spec"
 
 
 @pytest.fixture
-def cookie_data_path() -> Path:
+def cookie_data_path(test_data_path) -> Path:
     """Return the path to the test data directory."""
-    return Path(__file__).parent / "cookie_spec"
+    return test_data_path / "cookie_spec"
