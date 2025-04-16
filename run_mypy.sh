515ec4fa51c922a3944602a9091307fa9f64e7f7
Richard Fabian <raspo1@gmail.com>
2025-04-16 10:48:23 +0100

    move "mypy withs" to variable

--- run_mypy.sh
+++ run_mypy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+ADDITIONAL_WITHS="--with types-PyYAML --with pytest --with markdown-it-py --with mdformat --with types-click"
+
 while true; do
     # rerun script whenever anything changes in the spicy or tests folders
     find src/spicy tests | sed '/__pycache__/d' > .ruff_files
-    cat .ruff_files | entr -d bash -c "clear && uvx --with types-PyYAML --with pytest --with types-click mypy --strict src/spicy tests"
+    cat .ruff_files | entr -d bash -c "clear && uvx ${ADDITIONAL_WITHS} mypy --strict src/spicy tests"
     rm .ruff_files
     # sleep for four seconds so we have a chance to quit
     sleep 4
