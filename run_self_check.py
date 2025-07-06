2d4f528dd6ba2647d2eec6a53b33ed0e37321b98
Richard Fabian <raspo1@gmail.com>
2025-07-06 14:44:41 +0100

    change from bash to python scripts to run tests

---
+++ run_self_check.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Looping runner for spicy self check."""
+
+import subprocess
+import time
+from pathlib import Path
+
+
+def main() -> None:
+    """Continuously rerun test when files in src/spicy or docs change."""
+    watch_dirs = ["src/spicy", "docs"]
+    self_check_files = Path(".self_check_files")
+    while True:
+        files = [
+            str(path)
+            for d in watch_dirs
+            for path in Path(d).rglob("*")
+            if "__pycache__" not in str(path) and path.is_file()
+        ]
+        self_check_files.write_text("\n".join(files))
+        try:
+            subprocess.run(
+                [
+                    "entr",
+                    "-d",
+                    "bash",
+                    "-c",
+                    "clear && uv run spicy docs",
+                ],
+                input="\n".join(files),
+                text=True,
+                check=True,
+            )
+        except subprocess.CalledProcessError:
+            pass
+        finally:
+            if self_check_files.is_file():
+                self_check_files.unlink()
+        time.sleep(4)
+
+
+if __name__ == "__main__":
+    main()
