672ec6afee7bbfc1d981cb243c151ffda1fd3212
Richard Fabian <raspo1@gmail.com>
2025-11-01 17:33:31 +0000

    much better link checking and even fix suggestions

--- src/spicy/md_link_check.py
+++ src/spicy/md_link_check.py
@@ -2,39 +2,32 @@
 
 import re
 from collections import defaultdict
+from difflib import SequenceMatcher
 from pathlib import Path
+
+TARGETS_DICT = dict[str, tuple[Path, int]]
+REFS_DICT = defaultdict[str, list[tuple[Path, int]]]
 
 
 def check_markdown_refs(file_list: list[Path], *, base_path: Path, prefix: str, fix_refs: bool = False) -> list[str]:
     """Check matching refs are correctly linked, optionally fixing them."""
-    targets: dict[str, tuple[Path, int]] = {}
-    references: defaultdict[str, list[tuple[Path, int]]] = defaultdict(list)
-    re_section = get_section_pattern_from_prefix(prefix)
-    re_reference = get_section_reference_pattern_from_prefix(prefix)
-
-    for path in file_list:
-        content = path.read_text()
-        for target, line in get_matches_from_md(content, re_section).items():
-            targets[target] = (path, line)
-        for reference, line in get_matches_from_md(content, re_reference).items():
-            # skip any that are actually sections
-            if (path, line) in targets.values():
-                continue
-            references[reference].append((path, line))
+    targets, references = gather_markdown_sections_and_refs(file_list, prefix)
 
     expected_targets = {
         target: f"[{target}]({path.relative_to(base_path)}#{target.lower()})" for target, (path, _) in targets.items()
     }
+    valid_targets = list(targets)
 
     issue_list = []
     # check for completely invalid references
     for ref, list_of_locations in references.items():
-        expected = expected_targets[ref]
         for path, line in list_of_locations:
             if ref not in targets:
-                issue_list.append(f"Bad reference found: {ref} in {path}({line + 1}) has no matching section.")
-                issue_list.append(f"Possible targets = {targets.keys()}")
+                best_alternative = f"Did you mean {closest_string(ref, valid_targets)}"
+                issue = f"Bad reference found: {ref} in {path}({line + 1}) has no matching section."
+                issue_list.append(f"{issue} {best_alternative}")
             else:
+                expected = expected_targets[ref]
                 line_content = path.read_text().split("\n")[line]
                 re_link = get_link_pattern_from_reference(ref)
                 m = re_link.search(line_content)
@@ -56,6 +49,38 @@ def check_markdown_refs(file_list: list[Path], *, base_path: Path, prefix: str, 
                             f"Reference has bad link: {ref} in {path}({line + 1}) is {actual} but should be {expected}",
                         )
     return issue_list
+
+
+def closest_string(needle: str, haystack: list[str]) -> str:
+    """Find the closest matching string."""
+    best_guess = ""
+    best_score = 0.0
+    for guess in haystack:
+        score = SequenceMatcher(a=needle, b=guess).ratio()
+        if score > best_score:
+            best_score = score
+            best_guess = guess
+    return best_guess
+
+
+def gather_markdown_sections_and_refs(file_list: list[Path], prefix: str) -> tuple[TARGETS_DICT, REFS_DICT]:
+    """Gather the sections and all possible references."""
+    targets: dict[str, tuple[Path, int]] = {}
+    references: defaultdict[str, list[tuple[Path, int]]] = defaultdict(list)
+    re_section = get_section_pattern_from_prefix(prefix)
+    re_reference = get_section_reference_pattern_from_prefix(prefix)
+
+    for path in file_list:
+        content = path.read_text()
+        for target, line in get_matches_from_md(content, re_section).items():
+            targets[target] = (path, line)
+        for reference, line in get_matches_from_md(content, re_reference).items():
+            # skip any that are actually sections
+            if (path, line) in targets.values():
+                continue
+            references[reference].append((path, line))
+
+    return targets, references
 
 
 def update_line_in_file(
